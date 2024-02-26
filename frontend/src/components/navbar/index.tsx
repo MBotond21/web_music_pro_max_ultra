1d32710e4ce64a68174262ddb6dafcb9f79d5f23
Oláh András <106300817+oandris228@users.noreply.github.com>
2024-02-26 13:00:47 +0100

    update

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,20 +1,19 @@
 import { Navbar, Container, NavDropdown, Nav} from 'react-bootstrap'
+import "./index.css"
 
 interface Props {
 
 }
 
 export function MainNavbar(props: Props){
-    return <Navbar expand="lg" className="bg-body-tertiary">
+    return <Navbar expand="lg" className="gamer">
     <Container>
       <Navbar.Brand href="#home">Lorem ipsum</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link href="#home">Főoldal</Nav.Link>
           <h2>Műfajok</h2>
           <Nav.Link href="#link"></Nav.Link>
-          
         </Nav>
       </Navbar.Collapse>
     </Container>
