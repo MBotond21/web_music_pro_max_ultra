981d84ae5ebea6d60f273d2c199860862873e717
Oláh András <106300817+oandris228@users.noreply.github.com>
2024-03-01 10:08:50 +0100

    created navbar?

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -8,12 +8,15 @@ interface Props {
 export function MainNavbar(props: Props){
     return <Navbar expand="lg" className="gamer">
     <Container>
-      <Navbar.Brand href="#home">Lorem ipsum</Navbar.Brand>
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home">Főoldal</Nav.Link>
-          <h2>Műfajok</h2>
-          <Nav.Link href="#link"></Nav.Link>
+          <Nav.Link href="#home" className='navlink'>Főoldal</Nav.Link><br></br>
+          <Nav.Link href="#link" className='navlink'>Pop</Nav.Link><br></br>
+          <Nav.Link href="#link" className='navlink'>Rock</Nav.Link><br></br>
+          <Nav.Link href="#link" className='navlink'>Indie</Nav.Link><br></br>
+          <Nav.Link href="#link" className='navlink'>Metál</Nav.Link><br></br>
+          <Nav.Link href="#link" className='navlink'>Játék</Nav.Link><br></br>
+          <Nav.Link href="#link" className='navlink'>Hip-hop</Nav.Link><br></br>
         </Nav>
       </Navbar.Collapse>
     </Container>
