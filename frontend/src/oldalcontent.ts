f15e412c8fdd9a30143c9ab11921ef92ba7f7b15
P.Boti <botondpasztor@gmail.com>
2024-03-03 18:36:09 +0100

    template frameadatok json és hozzá interface létrehozása

---
+++ frontend/src/oldalcontent.ts
@@ -0,0 +1,12 @@
+export interface Genres {
+    genres: Genre[]
+}
+
+export interface Genre {
+    genre: string
+    title: string
+    leiras: string
+    categoria: string
+    spotify_playlist: string
+    eloadok: string[]
+}
