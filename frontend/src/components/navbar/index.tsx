0bfcb0e4362d643ae8c9185d7643bb44198b5f3e
Oláh András <106300817+oandris228@users.noreply.github.com>
2024-03-11 12:13:52 +0100

    another one

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -21,12 +21,12 @@ export function MainNavbar(props: Props){
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link href="#home" className='navlink'>Főoldal</Nav.Link><br/>
-          <Nav.Link onClick = {() => FrameUpdate("Pop")} className='navlink'>Pop</Nav.Link><br></br>
-          <Nav.Link onClick = {() => FrameUpdate("Rock")} className='navlink'>Rock</Nav.Link><br></br>
-          <Nav.Link onClick = {() => FrameUpdate("Indie")} className='navlink'>Indie</Nav.Link><br></br>
-          <Nav.Link onClick = {() => FrameUpdate("Metal")} className='navlink'>Metál</Nav.Link><br></br>
-          <Nav.Link onClick = {() => FrameUpdate("Jatek")} className='navlink'>Játék</Nav.Link><br></br>
-          <Nav.Link onClick = {() => FrameUpdate("Hip-hop")} className='navlink'>Hip-hop</Nav.Link><br></br>
+          <Nav.Link onClick = {() => FrameUpdate("Pop")} className='navlink pop'>Pop</Nav.Link><br></br>
+          <Nav.Link onClick = {() => FrameUpdate("Rock")} className='navlink rock'>Rock</Nav.Link><br></br>
+          <Nav.Link onClick = {() => FrameUpdate("Indie")} className='navlink indie'>Indie</Nav.Link><br></br>
+          <Nav.Link onClick = {() => FrameUpdate("Metal")} className='navlink metal'>Metál</Nav.Link><br></br>
+          <Nav.Link onClick = {() => FrameUpdate("Jatek")} className='navlink jatek'>Játék</Nav.Link><br></br>
+          <Nav.Link onClick = {() => FrameUpdate("Hip-hop")} className='navlink hiphop'>Hip-hop</Nav.Link><br></br>
         </Nav>
       </Navbar.Collapse>
     </Container>
